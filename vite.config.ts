1c89c3e518c516372893a4aa85b4de3634d8cd66
abdulahadweb <abdulahadweb1@gmail.com>
2026-06-27 22:22:29 +0500

    feat: resolve environment configurations and verify local agent runtime

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Lecture-3-tasks/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
